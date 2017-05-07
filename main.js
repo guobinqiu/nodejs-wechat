eeaca47c69357a64b0008e0130ac3152bfde8db1
Guobin <qracle@126.com>
2017-05-07 16:42:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let Wechat = require('./wechat');
 let config = {
   app_id: 'YOUR_APP_ID',
   app_secret: 'YOUR_APP_SECRET',
-}
+};
 
 let wechat = new Wechat(config);
 wechat.getUserSummary("2017-05-01", "2017-05-05").then(data => {
