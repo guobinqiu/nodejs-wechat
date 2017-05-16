3ae6e73dc18852e6690adb1e0ad68a6a4342c030
Guobin <qracle@126.com>
2017-05-16 17:16:34 +0800

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const wechat_1 = require("./wechat");
 let config = {
-    app_id: 'wxdc02ed9004592908',
-    app_secret: '2ecd9ee0f11b5444292317b8ef821c98',
+    app_id: '',
+    app_secret: '',
 };
 let wechat = new wechat_1.Wechat(config);
 wechat.getUserSummary("2017-05-01", "2017-05-05").then(data => {
